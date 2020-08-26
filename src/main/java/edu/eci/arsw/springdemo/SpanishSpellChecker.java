69420308f61eaed9bdb987dacb6f7a7ca29ae656
JuanchoG <juan.garcia-ga@mail.escuelaing.edu.co>
2020-08-26 13:23:25 -0500

    Primera parte

---
+++ src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
@@ -0,0 +1,18 @@
+package edu.eci.arsw.springdemo;
+
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Component("SpanishSpell")
+public class SpanishSpellChecker implements SpellChecker {
+
+	@Override
+	public String checkSpell(String text) {
+		return "revisando ("+text+") con el verificador de sintaxis del espanol";
+                
+                
+	}
+
+}

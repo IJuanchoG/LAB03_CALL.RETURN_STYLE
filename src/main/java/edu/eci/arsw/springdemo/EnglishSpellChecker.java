6a676ecabf9da4fe9c627258d5d5d9c6eb65590f
JuanchoG <juan.garcia-ga@mail.escuelaing.edu.co>
2020-09-02 00:01:38 -0500

    Readme Terminado

--- src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
@@ -2,6 +2,9 @@ package edu.eci.arsw.springdemo;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * The type English spell checker.
+ */
 @Component("EnglishSpell")
 public class EnglishSpellChecker implements SpellChecker {
 
--- src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
@@ -4,25 +4,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+/**
+ * The type Grammar checker.
+ */
 @Service
 public class GrammarChecker {
 
+	/**
+	 * The Sc.
+	 */
 	@Autowired
 	@Qualifier("SpanishSpell")
 	SpellChecker sc;
 
+	/**
+	 * The X.
+	 */
 	String x;
-        
-        
+
+
+	/**
+	 * Gets spell checker.
+	 *
+	 * @return the spell checker
+	 */
 	public SpellChecker getSpellChecker() {
 		return sc;
 	}
 
+	/**
+	 * Sets spell checker.
+	 *
+	 * @param sc the sc
+	 */
 	public void setSpellChecker(SpellChecker sc) {
 		this.sc = sc;
 	}
 
 
+	/**
+	 * Check string.
+	 *
+	 * @param text the text
+	 * @return the string
+	 */
 	public String check(String text){
 		
 		StringBuffer sb=new StringBuffer();
